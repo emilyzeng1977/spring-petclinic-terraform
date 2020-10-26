ee38a7684a1f426cf4fd015f6de087583d185a0f
tniu <tniu@infomedia.com.au>
2020-10-27 07:45:44 +1100

    fix s3

--- fixtures.ap-southeast-2.tfvars
+++ fixtures.ap-southeast-2.tfvars
@@ -61,7 +61,3 @@ autoscale_upper_increment = 1
 elb_scheme = "public"
 
 solution_stack_name = "64bit Amazon Linux 2018.03 v2.11.0 running Java 8"
-
-version_label = ""
-
-dns_zone_id = "Z03490151F9W0LKB1NFT6"
--- main.tf
+++ main.tf
@@ -51,7 +51,6 @@ module "elastic_beanstalk_environment" {
   description                = var.description
   region                     = var.region
   availability_zone_selector = var.availability_zone_selector
-  dns_zone_id                = var.dns_zone_id
 
   wait_for_ready_timeout             = var.wait_for_ready_timeout
   elastic_beanstalk_application_name = module.elastic_beanstalk_application.elastic_beanstalk_application_name
