5badf9563f1e910fac5d3076f372df256d9437fd
tniu <tniu@infomedia.com.au>
2020-11-09 09:33:35 +1100

    fix ELB error

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "subnets" {
   igw_id               = module.vpc.igw_id
   cidr_block           = module.vpc.vpc_cidr_block
   nat_gateway_enabled  = false
-  nat_instance_enabled = false
+  nat_instance_enabled = true
 }
 
 module "elastic_beanstalk_application" {
